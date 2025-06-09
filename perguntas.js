81d6dabfe218c19e2826a0ffa442a7fe9399faa6
pyetro738 <pyetro.lopes@escola.pr.gov.br>
2025-06-09 09:20:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    criaCartao(
+        'Velocidade', 
+        'Qual é a maior velocidade já alcançada pelo flash?', 
+        '3.897.302.954.000.000.000.000.000m/s ou 140.202.906.344.000.000.000km/h aproximadamente (cálculo feito por fãs)'
+    );
+
+    criaCartao(
+        'Liga da Justiça sem Limites', 
+        'Qual era o nome da pessoa que assume o manto de flash no desenho Liga da Justiça Sem Limites?', 
+        'Wally West.'
+    );
+
+    criaCartao(
+        'Vilão', 
+        'Quem é o vilão principal que confronta flash?', 
+        'Seu arqui-inimigo, Flash Reverso'
+    );
+
+    criaCartao(
+        'História', 
+        'Como Barry Allen conseguiu seus poderes nos quadrinhos?', 
+        'Ele conseguiu seus poderes em seu laboratório, após ser exposto a produtos químicos e atingido por um raio.'
+    );
+});
